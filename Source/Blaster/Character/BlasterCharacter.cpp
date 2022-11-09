099b4fb135154946140cc75c74947d7053846be9
kmy9317 <kimmin9632@gmail.com>
2022-11-09 21:52:35 +0900

    Create SpeedPickup Class And Apply to BuffComponent

--- Source/Blaster/Character/BlasterCharacter.cpp
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -265,6 +265,7 @@ void ABlasterCharacter::PostInitializeComponents()
 	if (Buff)
 	{
 		Buff->Character = this;
+		Buff->SetInitialSpeeds(GetCharacterMovement()->MaxWalkSpeed, GetCharacterMovement()->MaxWalkSpeedCrouched);
 	}
 }
 
