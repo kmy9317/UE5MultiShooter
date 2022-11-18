f8fd803a6064009f7d9632ecc2c75ac35c29a64c
kmy9317 <kimmin9632@gmail.com>
2022-11-18 13:12:52 +0900

    Create TeamsGameMode

--- Source/Blaster/GameMode/TeamsGameMode.h
+++ Source/Blaster/GameMode/TeamsGameMode.h
@@ -3,18 +3,21 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/GameMode.h"
-#include "LobbyGameMode.generated.h"
+#include "BlasterGameMode.h"
+#include "TeamsGameMode.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class BLASTER_API ALobbyGameMode : public AGameMode
+class BLASTER_API ATeamsGameMode : public ABlasterGameMode
 {
 	GENERATED_BODY()
 	
 public:
 	virtual void PostLogin(APlayerController* NewPlayer) override;
-	
+	virtual void Logout(AController* Exiting) override;
+
+protected:
+	virtual void HandleMatchHasStarted() override;
 };
